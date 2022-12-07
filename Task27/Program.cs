1a783ff7cdcf1665811aa1e9922add5ad0ec0571
Inna Kirianova <innr@ya.ru>
2022-12-08 00:04:24 +0300

    added code for task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе/
+// 452 -> 11
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int total = 0;
+while (number != 0)
+{
+    total = total + (number % 10);
+    number = number/10;
+}
+
+Console.WriteLine($"сумма цифр в числе = {total}");
