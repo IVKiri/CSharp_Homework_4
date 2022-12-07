f6a6e4b6fff05bf836b02dcea59cb622808ccbf0
Inna Kirianova <innr@ya.ru>
2022-12-08 00:06:36 +0300

    added code for task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+
+Console.Write("На сколько элементов Вам требуется массив? Введите число: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [size];
+for (int i = 0; i < array.Length; i++)
+{
+     array[i] = new Random().Next(100);
+ }
+ Console.Write("[" + string.Join(", ", array) + "]");
